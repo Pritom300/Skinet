93c24c0c70a491a25146e1b3e93d593321f73d3c
Pritom <pritomsarkar300@gmail.com>
2022-07-13 15:17:26 +0600

    Adding pagination Functionlity without dynamic

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { IBrand } from '../shared/models/brand';
 import { IProduct } from '../shared/models/product';
 import { IType } from '../shared/models/productType';
+import { ShopParams } from '../shared/models/shopParams';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -15,9 +16,8 @@ export class ShopComponent implements OnInit {
   products : IProduct[];
   brands: IBrand[];
   types: IType[];
-  brandIdSelected = 0;
-  typeIdSelected = 0;
-  sortSelected = 'name';
+  shopParams = new ShopParams();
+  totalCount: number;
   sortOptions = [
     {name: 'Alphabetical', value: 'name'},
     {name: 'Price: Low to high', value: 'priceAsc'},
@@ -37,8 +37,11 @@ export class ShopComponent implements OnInit {
 
   getProducts() {
 
-    this.shopService.getProducts(this.brandIdSelected,this.typeIdSelected,this.sortSelected).subscribe(response => {
+    this.shopService.getProducts(this.shopParams).subscribe(response => {
       this.products = response.data;
+      this.shopParams.pageNumber = response.pageIndex;
+      this.shopParams.pageSize = response.pageSize;
+      this.totalCount = response.count;
     }, error=>{
       console.log(error);
     })
@@ -63,17 +66,17 @@ export class ShopComponent implements OnInit {
   }
 
   onBrandSelected(brandId: number) {
-   this.brandIdSelected=brandId;
+   this.shopParams.brandId=brandId;
    this.getProducts();
   }
 
   onTypeSelected(typeId: number) {
-    this.typeIdSelected = typeId;
+    this.shopParams.typeId = typeId;
     this.getProducts();
   }
 
   onSortSelected(sort: string) {
-   this.sortSelected = sort;
+   this.shopParams.sort = sort;
    this.getProducts();
 
   }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -4,6 +4,7 @@ import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import { delay, map } from 'rxjs/operators';
+import { ShopParams } from '../shared/models/shopParams';
 
 @Injectable({
   providedIn: 'root'
@@ -14,23 +15,23 @@ export class ShopService {
 
   constructor(private http:HttpClient) { }
 
-  getProducts(brandId?:number,typeId?:number,sort?:string)
+  getProducts(shopParams:ShopParams)
   {
     let params = new HttpParams();
 
-    if(brandId)
+    if(shopParams.brandId)
     {
-      params = params.append('brandId',brandId.toString());
+      params = params.append('brandId',shopParams.brandId.toString());
     }
 
-    if(typeId)
+    if(shopParams.typeId)
     {
-      params = params.append('typeId',typeId.toString());
+      params = params.append('typeId',shopParams.typeId.toString());
     }
 
-    if(sort)
+    if(shopParams.sort)
     {
-      params = params.append('sort',sort);
+      params = params.append('sort',shopParams.sort);
     }
     
     return this.http.get<IPagination>(this.baseUrl + 'products',{observe:'response', params})
